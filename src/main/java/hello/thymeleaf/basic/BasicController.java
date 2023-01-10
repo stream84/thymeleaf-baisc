c4d01a8953dc456a2d5887010a74074a94931c12
myeongseok.seo <myeongseok.seo@softcamp.co.kr>
2023-01-10 14:09:53 +0900

    타임리프 -기본 2

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -55,12 +56,29 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
             return "Hello " + data;
         }
     }
+
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
 
     @Data
     static class User {
